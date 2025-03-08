91b90f21557c58e49115f86d7bf7c4ded7c16226
jrhe123 <keith_he@outlook.com>
2025-03-08 15:17:54 -0500

    mongodb replicas

---
+++ ordering-app/apps/orders/src/orders.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from '@app/common';
+import { OrdersRepository } from './orders.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OrderSchema } from './schemas/order.schema';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([
+      {
+        name: 'Order',
+        schema: OrderSchema,
+      },
+    ]),
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService, OrdersRepository],
+})
+export class OrdersModule {}
---
+++ readme.md
@@ -0,0 +1,10 @@
+### Setup Microservices
+1. nest new ordering-app
+2. nest generate app orders
+3. nest generate app billing
+4. nest generate app auth
+5. nest g library common
+
+5. npm run start:dev
+6. npm run start:dev billing
+7. npm run start:dev auth
