bb5203bc174a9255db58c04409c5852b0d08141e
jrhe123 <keith_he@outlook.com>
2025-03-08 19:34:05 -0500

    mq in order created & billing

--- ordering-app/apps/orders/src/orders.service.ts
+++ ordering-app/apps/orders/src/orders.service.ts
@@ -1,12 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateOrderRequest } from './dto/create-order.request';
 import { OrdersRepository } from './orders.repository';
+import { BILLING_SERVICE } from './constants/services';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class OrdersService {
-  constructor(private readonly ordersRepository: OrdersRepository) {}
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
   async createOrder(request: CreateOrderRequest) {
-    this.ordersRepository.create(request);
+    const session = await this.ordersRepository.startTransaction();
+    try {
+      const order = this.ordersRepository.create(request, { session });
+      // Observable -> promise: lastValueFrom
+      // "billingClient.emit" is a observable, we need to convert it to promise
+      await lastValueFrom(
+        this.billingClient.emit('order_created', {
+          request,
+        }),
+      );
+      await session.commitTransaction();
+      return order;
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    }
   }
 
   async getOrders() {
--- ordering-app/libs/common/src/rmq/rmq.module.ts
+++ ordering-app/libs/common/src/rmq/rmq.module.ts
@@ -1,8 +1,39 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { RMQService } from './rmq.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+
+interface RMQModuleOptions {
+  name: string;
+}
 
 @Module({
   providers: [RMQService],
   exports: [RMQService],
 })
-export class RMQModule {}
+export class RMQModule {
+  static register({ name }: RMQModuleOptions): DynamicModule {
+    return {
+      module: RMQModule,
+      imports: [
+        ClientsModule.registerAsync([
+          {
+            name,
+            useFactory: (configService: ConfigService) => ({
+              transport: Transport.RMQ,
+              options: {
+                urls: [configService.get<string>('RABBIT_MQ_URI')],
+                queue: configService.get<string>(`RABBIT_MQ_${name}_QUEUE`),
+                queueOptions: {
+                  durable: true,
+                },
+              },
+            }),
+            inject: [ConfigService],
+          },
+        ]),
+      ],
+      exports: [ClientsModule],
+    };
+  }
+}
